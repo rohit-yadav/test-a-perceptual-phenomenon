bda165e81e91020517eec6f636ea54e92b26e65d
Rohit Yadav <rohityadav2@outlook.com>
2018-07-16 21:22:43 +0530

    Import modules

--- test-a-perceptual-phenomenon.py
+++ test-a-perceptual-phenomenon.py
@@ -1,2 +1,17 @@
 
 # coding: utf-8
+
+# # Analyzing the Stroop Effect
+
+# In[1]:
+
+
+# Import Modules
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+import seaborn as sns
+import scipy.stats as stats
+
+get_ipython().run_line_magic('matplotlib', 'inline')
+
