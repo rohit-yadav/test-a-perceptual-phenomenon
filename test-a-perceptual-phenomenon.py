5d2b56a3a092d6f72531930af7bcc623e5447d71
Rohit Yadav <rohityadav2@outlook.com>
2018-07-16 21:40:45 +0530

    Visualize the data

--- test-a-perceptual-phenomenon.py
+++ test-a-perceptual-phenomenon.py
@@ -28,8 +28,53 @@ df = pd.read_csv('stroopdata.csv')
 df.head()
 
 
-# In[ ]:
+# In[6]:
 
 
-df.describe
+df.describe()
+
+
+# **A) Central Tendency**
+# 
+# 1. Mean of congruent = 14.05 and Mean of incongruent = 22.02
+# 2. Median of congruent = 14.35 and Median of incongruent = 21.01
+# 3. Mode of congruent = 22.33 and Mode of incongruent = 35.26
+
+# **B) Variability**
+# 
+# 1. Standard Deviation of congruent = 3.56 and that of incongruent is 4.80
+
+# In[7]:
+
+
+sns.set(color_codes=True)
+
+
+# In[8]:
+
+
+# Visualizations here
+sns.distplot(df['Congruent'])
+plt.show()
+
+
+# In[9]:
+
+
+sns.distplot(df['Incongruent'])
+plt.show()
+
+
+# In[10]:
+
+
+plt.boxplot(df['Incongruent'])
+plt.show()
+
+
+# In[11]:
+
+
+plt.boxplot(df['Congruent'])
+plt.show()
 
